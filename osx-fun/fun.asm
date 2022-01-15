0c669e5ddf2405a0ce8f221f504336c5cffba8ab
Maciej Smolinski <contact@maciejsmolinski.com>
2022-01-15 23:45:57 +0100

    Construct new user and build skeleton for printing

--- osx-fun/fun.asm
+++ osx-fun/fun.asm
@@ -3,20 +3,27 @@
 %define stdout 1
 %define success 0
 
+	default rel
+
 	section .bss
 
 	struc user
 		.id:	resq 1
-		.name: 	resb 255
+	    .name: 	resb 32
 	endstruc
+
+    users:	resw 1
 
 	section .text
 	global _main
 
 	extern _printf
+	extern _malloc
 
 _main:
 	call print_welcome_message
+	call new_user
+	call print_user
 	call exit
 
 print_welcome_message:
@@ -25,12 +32,37 @@ print_welcome_message:
 	.msg db "Welcome!", 0
 
 	section .text
-
 	mov rdi, .fmt
 	mov rsi, .msg
 	mov rax, 0
 	call _printf
 	ret
+
+new_user:
+	section .data
+	.username db "John", 0
+	.fmt db "%s", 10, 0
+
+	section .text
+	mov rdi, user_size
+	call _malloc
+	mov rdi, rax
+	mov [users], rdi
+    mov [rax+user.id], byte 1
+	mov rsi, .username
+	mov [rax+user.name], rsi
+	ret
+
+print_user:
+	section .data
+	.fmt db "%s", 10, 0
+
+	section .text
+
+	mov rdi, .fmt
+	mov rsi, [users+user.name]
+	mov rax, 0
+	call _printf
 
 exit:
 	mov rax, sys_exit
