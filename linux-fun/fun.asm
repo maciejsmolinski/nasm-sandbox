9e531ddc29f4cfa6f7ea041c761f1cb2c46e0816
Maciej Smolinski <contact@maciejsmolinski.com>
2022-01-22 00:54:40 +0000

    Format the output nicely (newline, colors)
    Useful resources:
    - https://misc.flogisoft.com/bash/tip_colors_and_formatting

--- linux-fun/fun.asm
+++ linux-fun/fun.asm
@@ -6,9 +6,9 @@
 	section .bss
 
 	struc user
-            .id:	resq 1
+	    .id:	resq 1
 	    align	4
-            .name: 	resb 64
+	    .name:	resb 64
 	    align	8
 	endstruc
 
@@ -24,16 +24,16 @@ main:
 	call print_intro
 	call new_user
 	call print_user
+	call print_newline
 	call exit
 
 print_intro:
 	section .data
-	
-.fmt 	db "%s", 10, 0
-.msg 	db "[Writing to and reading from memory]", 10, 0
+.fmt	db "%s", 10, 0
+.msg	db  `\e[90m// Exercise: Writing to and reading from memory\e[0m`, 10, 0
 
 	section .text
-	
+
 	mov rdi, .fmt
 	mov rsi, .msg
 	mov rax, 0
@@ -42,26 +42,24 @@ print_intro:
 
 new_user:
 	section .data
-	
-.name 	db "Paul Tray", 0
+.name	db "Paul Tray", 0
 
 	section .text
-	
+
 	mov rdi, user_size
 	call malloc
-	
+
 	mov [users], rax
 	mov [rax+user.id], word 256
 
-	lea rsi, [.name]	
+	lea rsi, [.name]
 	lea rdi, [rax+user.name]
 	call strcpy
 	ret
 
 print_user:
 	section .data
-	
-.fmt 	db 'User<{ name: "%s", id: %ld }>', 10, 0
+.fmt	db `-> \e[0mUser\e[0m<\e[90m{ name: \e[36m"%s"\e[0;90m, id: \e[36m%ld\e[0m }\e[0m>`, 10,  0
 
 	section .text
 
@@ -75,8 +73,18 @@ print_user:
 
 	ret
 
+print_newline:
+	section .data
+.fmt	db 10, 0
+
+	section .text
+	xor rax, rax
+	mov rdi, .fmt
+	call printf
+
+	ret
+
 exit:
 	mov rax, sys_exit
 	mov rdi, success
 	syscall
-
